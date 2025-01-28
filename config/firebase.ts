fcf13d3918e20d89aa5138ebac38f02a764fd463
kirdua <kirdua@gmail.com>
2025-01-28 16:57:00 -0600

    update

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDHWhZ0YCIvybDzR-v0uBoJa9k3YOKk2wA',
-  authDomain: 'expense-tracker-f0d2c.firebaseapp.com',
-  projectId: 'expense-tracker-f0d2c',
-  storageBucket: 'expense-tracker-f0d2c.firebasestorage.app',
-  messagingSenderId: '454350863249',
-  appId: '1:454350863249:web:30ad8e2e5b0ad62b3e7830'
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 }
 
 // Initialize Firebase
