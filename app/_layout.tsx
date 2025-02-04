96e4ef2da8a15fe6592fa8b134d5badc4f9bf2b7
kirdua <kirdua@gmail.com>
2025-02-04 10:18:42 -0600

    implementing home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,6 +14,10 @@ const StackLayout = () => {
         name='(modals)/walletModal'
         options={{ presentation: 'modal' }}
       ></Stack.Screen>
+      <Stack.Screen
+        name='(modals)/transactionModal'
+        options={{ presentation: 'modal' }}
+      ></Stack.Screen>
     </Stack>
   )
 }
